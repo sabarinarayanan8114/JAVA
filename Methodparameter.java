ae70d633d7e5539af4838a56da382a3bbbbb5948
sabarinarayanan8114 <dsabari2408@gmail.com>
2025-09-13 14:27:11 +0530

    Method with Paramater

---
+++ Methodparameter.java
@@ -0,0 +1,15 @@
+
+package array;
+import java.util.*;
+public class Methodparameter {
+	 static void add(int a,int b) {
+System.out.println(a+b);
+}
+	public static void main(String[]args) {
+		Scanner kbd=new Scanner(System.in);
+		int n1=kbd.nextInt();
+		int n2=kbd.nextInt();
+		add(n1,n2);
+				}
+	}
+	
