355814b1ca0c6c694baff69e0c3bca15430a0551
sabarinarayanan8114 <dsabari2408@gmail.com>
2025-09-13 14:29:34 +0530

    Method without parameter and returntype

---
+++ WithoutParameterReturntype.java
@@ -0,0 +1,16 @@
+package array;
+
+import java.util.Scanner;
+
+public class WithoutParameterReturntype {
+   static void add(){
+	   Scanner kbd=new Scanner(System.in);
+	   int n1=kbd.nextInt();
+	   int n2=kbd.nextInt();
+	   System.out.println(n1+n2);
+   }
+   public static void main(String[] args) {
+	add();
+}
+}
+
